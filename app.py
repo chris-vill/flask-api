88100e6ae8d58cb04261bc29ba5b6a1ecaea3161
Christopher Villaran <cv.career.github.v0.pgs9v@simplelogin.com>
2025-02-20 21:09:18 +0800

    master: added GET & POST endpoints for stores and store items

--- app.py
+++ app.py
@@ -1,12 +1,78 @@
-from flask import Flask
+"""
+TODO
+1. fix formatting
+2. fix location of app.py
+3. fix auto initializing of venv when opening a terminal
+"""
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99,
+            }
+        ],
+    }
+]
 
-@app.route("/")
-def hello_world():
-    return "Hello, Dockerized Flask!"
+
+@app.get("/stores")
+def get_stores():
+    return {"stores": stores}, 200
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.post("/stores")
+def post_stores():
+    request_data = request.get_json()
+    new_store = {
+        "name": request_data["name"],
+        "items": [],
+    }
+    stores.append(new_store)
+    return new_store, 201
+
+
+@app.get("/stores/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store, 200
+    return {
+        "message": "Store not found",
+    }, 404
+
+
+@app.get("/stores/<string:name>/items")
+def get_store_items(name):
+    for store in stores:
+        if store["name"] == name:
+            return {
+                "items": store["items"],
+            }
+    return {
+        "message": "Store not found",
+    }, 404
+
+
+@app.post("/stores/<string:name>/items")
+def post_store_item(name):
+    request_data = request.get_json()
+
+    for store in stores:
+        if store["name"] == name:
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"],
+            }
+            store["items"].append(new_item)
+            return new_item, 201
+
+    return {
+        "message": "Store not found",
+    }, 404
